8f792b1799377d486ec1b575a14c13051c766013
ehsanmoodi <ehsanmoodi90@gmail.com>
2020-07-10 06:10:15 +0430

    using list rendering to show notes

--- react/00-enter-react-world/05-list-rendering/src/components/StickyNote.jsx
+++ react/00-enter-react-world/05-list-rendering/src/components/StickyNote.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import Note from './Note';
 
 export default function StickyNote() {
+    
+    const notesData  = [
+        { title: 'Title #1', content: 'Text Content #1' },
+        { title: 'Title #2', content: 'Text Content #2' },
+        { title: 'Title #3', content: 'Text Content #3' },
+        { title: 'Title #4', content: 'Text Content #4' },
+        { title: 'Title #5', content: 'Text Content #5' },
+        { title: 'Title #6', content: 'Text Content #6' }
+    ];
+
+    const notes = notesData.map(
+        (note, index) => <Note title={note.title} content={note.content} key={index} />
+    );
+    
+        
     return (
         <ul>
-            <Note title="Title #1" content="Text Content #1" />
-            <Note title="Title #2" content="Text Content #2" />
-            <Note title="Title #3" content="Text Content #3" />
-            <Note title="Title #4" content="Text Content #4" />
-            <Note title="Title #5" content="Text Content #5" />
-            <Note title="Title #6" content="Text Content #6" />
+            { notes }
         </ul>
     );
 }
